e99d5259e21b75fcd31e48f770b9306ff297bc08
sidorenko-d-web <sidorenko.d.web@gmail.com>
2024-08-11 04:30:01 +0500

    deleted yandex maps and made text overflow for item names

--- client/src/components/catalog-components/catalogItem.jsx
+++ client/src/components/catalog-components/catalogItem.jsx
@@ -30,7 +30,7 @@ const CatalogItem = ({ id, title, cost, itemImage }) => {
         <div className="w-full flex flex-col gap-3">
             <div className=" rounded-2xl md:rounded-xl 2xl:rounded-3xl bg-c-white shadow-c-sh p-3 2xl:p-6   flex flex-col justify-between gap-2" >
                 <a href={`/item?id=${id}`}> <img className='w-full rounded-lg aspect-square' src={itemImage} alt="item" /></a>
-                <h3 className=" leading-none text-right text-xl 2xl:text-2xl font-semibold ">{title}</h3>
+                <h3 className=" leading-none text-right text-xl 2xl:text-2xl font-semibold whitespace-nowrap overflow-ellipsis overflow-hidden">{title}</h3>
                 <p className=" text-right text-3xl md:text-2xl 2xl:text-4xl font-bold">{cost}р</p>
             </div>
             <button className="bg-c-green active:bg-green-900 hover:bg-green-700 transition-all text-xl md:text-base 2xl:text-2xl rounded-2xl md:rounded-xl 2xl:rounded-3xl w-full text-c-white px-2 py-2 leading-1 md:leading-none font-semibold shadow-c-sh" onClick={handleAddItemToCart}>Добавить в<br />корзину</button>
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,88 +1,106 @@
-import React, { useEffect, useState,  } from 'react'
-
-import { YMaps, Map } from '@pbe/react-yandex-maps';
-import Header from './ui-break-points/header.jsx';
-import Footer from './ui-break-points/footer.jsx';
-import CatalogItem from './catalog-components/catalogItem.jsx';
-import AdminCatalogItem from './catalog-components/adminCatalogItem.jsx';
-import Pagination from './ui-components/pagination.jsx';
-import CatalogControls from './catalog-components/catalogControls.jsx';
-import CatalogAddItem from './catalog-components/catalogAddItem.jsx';
-import axios from 'axios';
-import Cookies from 'js-cookie'
+import React, { useEffect, useState } from "react";
+import Header from "./ui-break-points/header.jsx";
+import Footer from "./ui-break-points/footer.jsx";
+import CatalogItem from "./catalog-components/catalogItem.jsx";
+import AdminCatalogItem from "./catalog-components/adminCatalogItem.jsx";
+import Pagination from "./ui-components/pagination.jsx";
+import CatalogControls from "./catalog-components/catalogControls.jsx";
+import CatalogAddItem from "./catalog-components/catalogAddItem.jsx";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const Index = () => {
-    const [isAdminState, setIsAdminState] = useState(Cookies.get('Authorization') === 'administrator' ? true : false)
+  const [isAdminState, setIsAdminState] = useState(
+    Cookies.get("Authorization") === "administrator" ? true : false
+  );
 
-    const [page, setPage] = useState(1)
-    const [totalPages, setTotalPages] = useState()
-    const [items, setItems] = useState([])
-    const [sort, setSort] = useState('cost')
-    const [filter, setFilter] = useState('n')
-    const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const [items, setItems] = useState([]);
+  const [sort, setSort] = useState("cost");
+  const [filter, setFilter] = useState("n");
+  const [search, setSearch] = useState("");
 
-    const handleSearchSubmit = (e) => {
-        e.preventDefault()
-        setSearch(e.target.searchRequest.value)
-    }
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target.searchRequest.value);
+  };
 
-    useEffect(() => {
-        axios.get(`${import.meta.env.VITE_API_URL}/getallitems?`, {
-            params:{
-                page,
-                sort,
-                filter,
-                search
-            }
-        }).then(res => {
-            setItems(res.data.items)
-            if(res.data.count <= 12){
-                setTotalPages(1)
-            }else{
-                setTotalPages(Math.ceil(res.data.count / 12))
-            }
-        })
-    }, [page, sort, filter, search])
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/getallitems?`, {
+        params: {
+          page,
+          sort,
+          filter,
+          search,
+        },
+      })
+      .then((res) => {
+        setItems(res.data.items);
+        if (res.data.count <= 12) {
+          setTotalPages(1);
+        } else {
+          setTotalPages(Math.ceil(res.data.count / 12));
+        }
+      });
+  }, [page, sort, filter, search]);
 
-    return (
-        <div className="bg-c-bg text-c-black-500">
-            <Header/>
-            {/* catalog */}
-            <main className="flex flex-col items-center w-10/12  md:w-8/12 mx-auto">
-                <h1 className="text-5xl 2xl:text-6xl font-bold  py-10">Каталог</h1>
-                {/* search */}
-                <form className="bg-c-white 2xl:text-2xl flex justify-between w-full md:w-9/12 px-5 py-3 rounded-full shadow-c-sh" onSubmit={e => handleSearchSubmit(e)}>
-                    <input className="bg-c-white w-11/12 outline-none"  type="text" name='searchRequest' placeholder="Поиск" id="global-search" />
-                    <button className="w-6" type='submit' htmlFor="global-search"><img src="/magnifying-glass-solid.svg" alt="" /></button>
-                </form>
-                {/* catalog controls */}
-                <CatalogControls setSort={setSort} setFilter={setFilter}/>
-                {/* items */}
-                <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-3 2xl:gap-6 justify-between">
-                    {isAdminState && <CatalogAddItem />}
-                    {items.map((elem, index) => 
-                        isAdminState?
-                        <AdminCatalogItem id={elem._id} key={index} title={elem.title} cost={elem.cost} itemImage={elem.itemImage}/>
-                        :
-                            <CatalogItem id={elem._id} key={index} title={elem.title} cost={elem.cost} itemImage={elem.itemImage}/>
-                    )}
-                </div>
-                {/* pagination */}
-                <Pagination page={page} totalPages={totalPages} setPage={setPage}/>
-                {/* pick up points */}
-                <div className="flex flex-col w-full items-center">
-                    <div className="text-4xl font-bold  mb-10 text-center">Наши пункты выдачи</div>
-                    <YMaps className="w-[100vw]">
-                        <div className="w-[100vw]">
-                            <Map className="w-[100vw] h-96 md:h-52" defaultState={{ center: [56.834018, 60.597106], zoom: 15 }} />
-                        </div>
-                    </YMaps>
-                </div>
-            </main>
-
-           <Footer/>
+  return (
+    <div className="bg-c-bg text-c-black-500">
+      <Header />
+      {/* catalog */}
+      <main className="flex flex-col items-center w-10/12  md:w-8/12 mx-auto">
+        <h1 className="text-5xl 2xl:text-6xl font-bold  py-10">Каталог</h1>
+        {/* search */}
+        <form
+          className="bg-c-white 2xl:text-2xl flex justify-between w-full md:w-9/12 px-5 py-3 rounded-full shadow-c-sh"
+          onSubmit={(e) => handleSearchSubmit(e)}
+        >
+          <input
+            className="bg-c-white w-11/12 outline-none"
+            type="text"
+            name="searchRequest"
+            placeholder="Поиск"
+            id="global-search"
+          />
+          <button className="w-6" type="submit" htmlFor="global-search">
+            <img src="/magnifying-glass-solid.svg" alt="" />
+          </button>
+        </form>
+        {/* catalog controls */}
+        <CatalogControls setSort={setSort} setFilter={setFilter} />
+        {/* items */}
+        <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-3 2xl:gap-6 justify-between">
+          {isAdminState && <CatalogAddItem />}
+          {items.map((elem, index) =>
+            isAdminState ? (
+              <AdminCatalogItem
+                id={elem._id}
+                key={index}
+                title={elem.title}
+                cost={elem.cost}
+                itemImage={elem.itemImage}
+              />
+            ) : (
+              <CatalogItem
+                id={elem._id}
+                key={index}
+                title={elem.title}
+                cost={elem.cost}
+                itemImage={elem.itemImage}
+              />
+            )
+          )}
         </div>
-    )
-}
+        {/* pagination */}
+        <Pagination page={page} totalPages={totalPages} setPage={setPage} />
+        {/* pick up points */}
+      </main>
 
-export default Index
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
